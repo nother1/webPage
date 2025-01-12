033915dc1ad6b018c0fcf1eecf4ace728a1282ea
Jhonatan <jhonatanpereanez@gmail.com>
2025-01-12 17:08:19 -0500

    Dist

---
+++ frontend/src/App.vue.js
@@ -0,0 +1,41 @@
+function __VLS_template() {
+    const __VLS_ctx = {};
+    let __VLS_components;
+    let __VLS_directives;
+    ['logo', 'logo',];
+    // CSS variable injection 
+    // CSS variable injection end 
+    __VLS_elementAsFunction(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)({
+        ...{ class: ("flex items-center justify-center h-screen bg-gray-100") },
+    });
+    __VLS_elementAsFunction(__VLS_intrinsicElements.h1, __VLS_intrinsicElements.h1)({
+        ...{ class: ("text-4xl font-bold text-blue-500") },
+    });
+    ['flex', 'items-center', 'justify-center', 'h-screen', 'bg-gray-100', 'text-4xl', 'font-bold', 'text-blue-500',];
+    var __VLS_slots;
+    var $slots;
+    let __VLS_inheritedAttrs;
+    var $attrs;
+    const __VLS_refs = {};
+    var $refs;
+    var $el;
+    return {
+        attrs: {},
+        slots: __VLS_slots,
+        refs: $refs,
+        rootEl: $el,
+    };
+}
+;
+const __VLS_self = (await import('vue')).defineComponent({
+    setup() {
+        return {};
+    },
+});
+export default (await import('vue')).defineComponent({
+    setup() {
+        return {};
+    },
+    __typeEl: {},
+});
+; /* PartiallyEnd: #4569/main.vue */
